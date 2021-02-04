2451092e118a4c126c2daac9cd8220f4ff0f2ca5
Yubron <wlstn4966@gmail.com>
2021-02-04 16:20:50 +0900

    test

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/api/hello', function(req, res, next) {
   res.json([
-    {id: 1, username: "somebody"},
+    {id: 1, username: "test"},
     {id: 2, username: "somebody_else"}
   ]);
 });
